127742d19d6c98e7a4d8066802cf5aaad6dc359f
IamBusy <1342247033@qq.com>
2018-05-17 16:09:33 +0800

    Utils: add ad status constants

---
+++ src/js/utils/constants.js
@@ -0,0 +1,12 @@
+/**
+ * @author: william
+ * @contact: 1342247033@qq.com
+ * @site: http://www.xiaolewei.com
+ * @file: constants.js
+ * @time: 17/05/2018 15:03
+ */
+
+export const ADSTATUS = {
+    NORMAL: '投放中',
+    FINISH: '投放结束',
+};
